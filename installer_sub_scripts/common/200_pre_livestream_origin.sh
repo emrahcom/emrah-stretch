59c9a1ba0614c3e2e77c4dc3767bc4fc0440394b
emrah <emrah.com@gmail.com>
2017-07-11 17:26:27 +0300

    es-compiler: nginx-ts-module

--- installer_sub_scripts/common/200_pre_livestream_origin.sh
+++ installer_sub_scripts/common/200_pre_livestream_origin.sh
@@ -46,6 +46,35 @@ lxc-attach -n $MACH -- \
      apt $APT_PROXY_OPTION -y install ffmpeg
      apt $APT_PROXY_OPTION -y build-dep nginx"
 
+# nginx MPEG-TS live module
+ZIP="https://github.com/arut/nginx-ts-module/archive/master.zip"
+lxc-attach -n $MACH -- \
+    zsh -c \
+    "export DEBIAN_FRONTEND=noninteractive
+     mkdir -p /root/source
+     cd /root/source
+     setopt +o nomatch
+     rm -rf nginx_* nginx-* libnginx-mod-*
+     apt $APT_PROXY_OPTION source nginx
+     wget $ZIP -O master.zip
+     unzip master.zip"
+
+lxc-attach -n $MACH -- \
+    zsh -c \
+    'cd /root/source
+     NGINX_VERSION=$(ls nginx-[1-9].* -d)
+     mv nginx-ts-module-master $NGINX_VERSION/debian/modules/nginx-ts-module
+     sed -i "/--add-dynamic-module=.*headers-more-nginx-module/i \
+         \\\\t\t\t--add-module=\$(MODULESDIR)\/nginx-ts-module \\\\"
+     cd $NGINX_VERSION
+     dpkg-buildpackage -rfakeroot -uc -b
+
+     cd ..
+     mkdir -p /usr/local/ej/deb/livestream-origin
+     rm -f /usr/local/ej/deb/livestream-origin/libnginx-*.deb
+     rm -f /usr/local/ej/deb/livestream-origin/nginx-*.deb
+     mv libnginx-*.deb nginx-*.deb /usr/local/ej/deb/livestream-origin/'
+
 # -----------------------------------------------------------------------------
 # CONTAINER SERVICES
 # -----------------------------------------------------------------------------
