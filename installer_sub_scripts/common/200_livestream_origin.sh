4e3de31d11d394deb049c9577829624fe55fa579
emrah <emrah.com@gmail.com>
2017-07-12 14:12:03 +0300

    tipo: ej to es

--- installer_sub_scripts/common/200_livestream_origin.sh
+++ installer_sub_scripts/common/200_livestream_origin.sh
@@ -102,12 +102,12 @@ lxc-attach -n $MACH -- \
      dpkg -i /usr/local/es/deb/livestream-origin/nginx-doc_*.deb
      apt-mark hold nginx-common nginx-extras nginx-doc
 
-     mkdir -p /usr/local/ej/livestream/stat/
+     mkdir -p /usr/local/es/livestream/stat/
      gunzip -c /usr/share/doc/nginx-doc/examples/rtmp_stat.xsl.gz > \
-         /usr/local/ej/livestream/stat/rtmp_stat.xsl
-     chown www-data: /usr/local/ej/livestream/stat -R
-     ln -s hls /usr/local/ej/livestream/ts2hsl
-     ln -s dash /usr/local/ej/livestream/ts2dash"
+         /usr/local/es/livestream/stat/rtmp_stat.xsl
+     chown www-data: /usr/local/es/livestream/stat -R
+     ln -sf hls /usr/local/es/livestream/ts2hsl
+     ln -sf dash /usr/local/es/livestream/ts2dash"
 
 # -----------------------------------------------------------------------------
 # SYSTEM CONFIGURATION
